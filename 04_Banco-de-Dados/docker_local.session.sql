ac9fca06b16e40a45eb5592f09bef9ef1cab0965
peuborg <peuborg.8@gmail.com>
2024-10-03 12:09:47 -0300

    03_10_2024

--- 04_Banco-de-Dados/docker_local.session.sql
+++ 04_Banco-de-Dados/docker_local.session.sql
@@ -1,6 +1,8 @@
-CREATE TABLE produtos (
+/*Mysql*/
+/*CREATE TABLE produtos (
     id VARCHAR(255) NOT NULL,
     nome VARCHAR(80) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id)
-);
+);*/
+select * from produtos
