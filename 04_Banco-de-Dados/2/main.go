479b419bfd262c0f5d3bdd62eb61f04d9628def0
peuborg <peuborg.8@gmail.com>
2024-10-15 11:59:17 -0300

    15_10_24

--- 04_Banco-de-Dados/2/main.go
+++ 04_Banco-de-Dados/2/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,23 +14,64 @@ type Pessoa struct {
 }
 
 func main() {
+	//Open
 	dsn := "root:root@tcp(localhost:3306)/goexpert"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
+
 	//Create Table
 	db.AutoMigrate(&Pessoa{})
+
 	//Insert
-	db.Create(&Pessoa{
+	/*db.Create(&Pessoa{
 		Nome:  "Fulano de Tal",
 		Email: "fulano@email.com",
-	})
+	})*/
+
 	//Insert Many
 	pessoas := []Pessoa{
 		{Nome: "Beltrano", Email: "bel@email.com"},
 		{Nome: "Ciclano", Email: "ciclano@email.com"},
 		{Nome: "ze", Email: "ze@email.com"},
 	}
-	db.Create(pessoas)
+	//db.Create(pessoas)
+
+	//Select one
+	/*var pessoa Pessoa
+	//Seleciona o primeiro registro com o código do parâmetro
+	db.First(&pessoa, 1)
+	fmt.Println(pessoa)
+	//Seleciona o primeiro registro que satisfaz a condição
+	db.First(&pessoa, "nome = ?", "ze")
+	fmt.Println(pessoa)*/
+
+	//Select all
+	/*db.Find(&pessoas)
+	for _, pessoa := range pessoas {
+		fmt.Println(pessoa)
+	}*/
+
+	//Select os 5 primeiros registros
+	/*db.Limit(5).Find(&pessoas)
+	for _, pessoa := range pessoas {
+		fmt.Println(pessoa)
+	}*/
+
+	//Select os 5 primeiros registros da página 2
+	/*db.Limit(5).Offset(5).Find(&pessoas)
+	for _, pessoa := range pessoas {
+		fmt.Println(pessoa)
+	}*/
+
+	//WHERE
+	/*db.Where("id > ?", 5).Find(&pessoas)
+	for _, pessoa := range pessoas {
+		fmt.Println(pessoa)
+	}*/
+	db.Where("nome LIKE ?", "%Ciclano%").Find(&pessoas)
+	for _, pessoa := range pessoas {
+		fmt.Println(pessoa)
+	}
 }
